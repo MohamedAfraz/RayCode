c92ffd2ebdcb26a1fdb9c224b7f485aa39118794
Afraz <mohamedafraz19@gmail.com>
2024-02-23 18:14:21 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ const useStore = create(
       language: "plaintext",
       autoDetectLanguage: false,
       fontSize: 18,
-      fontStyle: "SFMono",
+      fontStyle: "robotoMono",
       padding: 64,
     }),
     {
